25aeea8402d0edbcdce66a96c24f40b9b9a0eac1
emsi06001 <emsi06001@E-PW0234N3>
2023-10-18 12:27:19 +0200

    Add project files.

---
+++ inluppg4.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace inluppg4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int highestNum = 0;
+            while (true)
+            {
+                Console.WriteLine("Skriv ett heltal. Skriv \"exit\" för att avsluta programmet");
+                string input = Console.ReadLine();
+                
+                if (input != "exit")
+                {
+                    int num = int.Parse(input);
+                    if (num > highestNum)
+                    {
+                        highestNum = num;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Högsta numret var " + highestNum);
+                    return;
+                }
+            }
+            
+        }
+    }
+}
